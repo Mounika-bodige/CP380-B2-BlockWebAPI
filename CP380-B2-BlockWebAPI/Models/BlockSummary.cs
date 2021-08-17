54b22d6e5649608270dc6b45dceee3bf000a28aa
Mounika-bodige <65857924+Mounika-bodige@users.noreply.github.com>
2021-08-17 19:59:52 -0400

    Update BlockSummary.cs

--- CP380-B2-BlockWebAPI/Models/BlockSummary.cs
+++ CP380-B2-BlockWebAPI/Models/BlockSummary.cs
@@ -1,12 +1,15 @@
-﻿using System;
+using CP380_B1_BlockList.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace CP380_B2_BlockWebAPI.Models
 {
-    public class BlockSummary
+    public class Blockdata
     {
-        // TODO
+        public DateTime TimeStamp { get; set; }
+        public string PreviousHash { get; set; }
+        public string Hash { get; set; }
     }
 }

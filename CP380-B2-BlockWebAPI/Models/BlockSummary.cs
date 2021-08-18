2c6c48dd92d7f54044ff0774df8766a8c63b065e
Mounika-bodige <65857924+Mounika-bodige@users.noreply.github.com>
2021-08-17 20:00:46 -0400

    Update BlockSummary.cs

--- CP380-B2-BlockWebAPI/Models/BlockSummary.cs
+++ CP380-B2-BlockWebAPI/Models/BlockSummary.cs
@@ -1,15 +1,78 @@
 using CP380_B1_BlockList.Models;
+using CP380_B2_BlockWebAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace CP380_B2_BlockWebAPI.Models
+namespace CP380_B2_BlockWebAPI.Controllers
 {
-    public class Blockdata
+    [Route("[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class BlocksController : ControllerBase
     {
-        public DateTime TimeStamp { get; set; }
-        public string PreviousHash { get; set; }
-        public string Hash { get; set; }
+        // TODO
+
+        private BlockList blcokl;
+
+        public BlocksController(BlockList blockList)
+        {
+            blcokl = blockList;
+        }
+
+
+        [HttpGet("/blocks")]
+        public IActionResult Get()
+        {
+            return Ok(blcokl.Chain.Select(block => new Blockdata()
+            {
+                Hash = block.Hash,
+                PreviousHash = block.PreviousHash,
+                TimeStamp = block.TimeStamp
+            }));
+        }
+
+        
+        [HttpGet("{hash}")]
+        public IActionResult GetBlock(string Hash)
+        {
+            var block = blcokl.Chain
+                .Where(block => block.Hash.Equals(Hash));
+
+            if (block != null && block.Count() > 0)
+            {
+                return Ok(block
+                    .Select(block => new Blockdata()
+                    {
+                        Hash = block.Hash,
+                        PreviousHash = block.PreviousHash,
+                        TimeStamp = block.TimeStamp
+                    }
+                    )
+                    .First());
+            }
+
+            return NotFound();
+        }
+
+        [HttpGet("{hash}/Payloads")]
+        public IActionResult GetBlockPayload(string Hash)
+        {
+            var block = blcokl.Chain
+                        .Where(block => block.Hash.Equals(Hash));
+
+            if (block != null && block.Count() > 0)
+            {
+                return Ok(block
+                    .Select(block => block.Data
+                    )
+                    .First());
+            }
+
+            return NotFound();
+        }
     }
 }

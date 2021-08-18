741ec2fddcd5d893602a5e8bb1cd96be16562230
Mounika-bodige <65857924+Mounika-bodige@users.noreply.github.com>
2021-08-17 20:01:07 -0400

    Update PendingPayloads.cs

--- CP380-B2-BlockWebAPI/Models/PendingPayloads.cs
+++ CP380-B2-BlockWebAPI/Models/PendingPayloads.cs
@@ -1,19 +1,36 @@
 using CP380_B1_BlockList.Models;
+using CP380_B2_BlockWebAPI.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace CP380_B2_BlockWebAPI.Models
+namespace CP380_B2_BlockWebAPI.Controllers
 {
-    public class Pendingpay
+    [Route("[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class PendingPayloadsController : ControllerBase
     {
-        public Pendingpay()
+        private Pendingpay pendingPayload;
+        public PendingPayloadsController(Pendingpay pendingPayloads)
         {
-            Payloads = new List<Payload>();
+            pendingPayload = pendingPayloads;
         }
 
-        public List<Payload> Payloads;
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(pendingPayload.Payloads);
+        }
+
+        [HttpPost]
+        public IActionResult Post(Payload payload)
+        {
+            pendingPayload.Payloads.Add(payload);
+            return Ok();
+        }
     }
 }
